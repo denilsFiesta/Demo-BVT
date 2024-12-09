b7d32c2151f50cdf48ac5f9089ed3612edec9247
denilsFiesta <denilson.fiesta@ucb.edu.bo>
2024-12-09 14:34:32 -0400

    especificacion de que incluye el catalogo

--- features/step_definitions/detailProduct.rb
+++ features/step_definitions/detailProduct.rb
@@ -1,12 +1,29 @@
 # Given I am on the GMO Post homepage
 Given(/^I am on the GMO Post homepage$/) do
   page.driver.browser.manage.window.maximize
-  visit('https://demo.borland.com/gmopost/')
+  visit('/')
 end
 
-# Then I should see the catalog
-Then(/^I should see the catalog$/) do
-  expect(page).to have_xpath("/html/body/table/tbody/tr/td[1]/h1")
+And(/^I should see the catalog with a list of products, including product names, prices, and item number$/) do
+  
+  base_xpath = '/html/body/form/table/tbody/tr[2]/td/div/center/table/tbody/tr'
+
+  rows = page.all(:xpath, "#{base_xpath}[position()>1]") 
+  expect(rows).not_to be_empty, 'No se encontraron filas en el catálogo'
+
+  rows.each_with_index do |row, index|
+    row_number = index + 2 
+    
+    item_number_xpath = "#{base_xpath}[#{row_number}]/td[1]"
+    expect(page).to have_selector(:xpath, item_number_xpath, text: /\S+/)
+
+    name_xpath = "#{base_xpath}[#{row_number}]/td[2]/a/strong"
+    expect(page).to have_selector(:xpath, name_xpath, text: /\S+/)
+
+    price_xpath = "#{base_xpath}[#{row_number}]/td[3]"
+    expect(page).to have_selector(:xpath, price_xpath, text: /\S+/)
+    
+  end
 end
 
 # And I click the "<product_name>" product
