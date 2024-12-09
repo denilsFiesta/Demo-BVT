fb9b7ed66a957b30cbed85e45747f5abd0cddcea
denilsFiesta <denilson.fiesta@ucb.edu.bo>
2024-12-09 17:13:32 -0400

    Limpieza de escenario no considerados para el BVT en detailProduct.feature

--- features/step_definitions/detailProduct.rb
+++ features/step_definitions/detailProduct.rb
@@ -4,28 +4,6 @@ Given(/^I am on the GMO Post homepage$/) do
   visit('/')
 end
 
-And(/^I should see the catalog with a list of products, including product names, prices, and item number$/) do
-  
-  base_xpath = '/html/body/form/table/tbody/tr[2]/td/div/center/table/tbody/tr'
-
-  rows = page.all(:xpath, "#{base_xpath}[position()>1]") 
-  expect(rows).not_to be_empty, 'No se encontraron filas en el catálogo'
-
-  rows.each_with_index do |row, index|
-    row_number = index + 2 
-    
-    item_number_xpath = "#{base_xpath}[#{row_number}]/td[1]"
-    expect(page).to have_selector(:xpath, item_number_xpath, text: /\S+/)
-
-    name_xpath = "#{base_xpath}[#{row_number}]/td[2]/a/strong"
-    expect(page).to have_selector(:xpath, name_xpath, text: /\S+/)
-
-    price_xpath = "#{base_xpath}[#{row_number}]/td[3]"
-    expect(page).to have_selector(:xpath, price_xpath, text: /\S+/)
-    
-  end
-end
-
 # And I click the "<product_name>" product
 When(/^I click the "(.*)" product$/) do |product_name|
   click_link(product_name)
