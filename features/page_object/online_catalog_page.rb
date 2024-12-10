72bb9fbeab8c6df22fab728c2619fdf8ebe01677
denilsFiesta <denilson.fiesta@ucb.edu.bo>
2024-12-10 19:51:03 -0400

    Esquelo de online_catalog_page.rb creado

---
+++ features/page_object/online_catalog_page.rb
@@ -0,0 +1,19 @@
+class OnlineCatalogPage < BasePage
+    def verify_page
+    end
+
+    def select_product(product_name)
+    end
+
+    def place_an_order
+    end
+
+    def reset_form
+    end
+
+    def product_list
+    end
+    
+    def set_quantity(product_name, qty)
+    end
+end
