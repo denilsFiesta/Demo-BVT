1ebf6394ca98f887c21f7b4b32c19646014bf9e2
denilsFiesta <denilson.fiesta@ucb.edu.bo>
2024-12-10 19:58:33 -0400

    Esquelo de place_order_page.rb creado

---
+++ features/page_object/place_order_page.rb
@@ -0,0 +1,19 @@
+class PlaceOrderPage < BasePage
+    def verify_page
+    end
+
+    def product_lines
+    end
+
+    def product_total
+    end
+
+    def sales_tax
+    end
+
+    def shipping_and_handling
+    end
+
+    def grand_total
+    end
+end
