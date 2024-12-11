b2b4b40ed760950c0ea8b9f6c9dffc15b0409fbd
denilsFiesta <denilson.fiesta@ucb.edu.bo>
2024-12-10 20:04:23 -0400

    Esquelo de online_store_receipt_page.rb creado

---
+++ features/page_object/online_store_receipt_page.rb
@@ -0,0 +1,31 @@
+class OnlineStoreReceiptPage < BasePage
+    def verify_page
+    end
+
+    def bill_to_info
+    end
+
+    def ship_to_info
+    end
+
+    def product_lines
+    end
+
+    def product_total
+    end
+
+    def sales_tax
+    end
+
+    def shipping_and_handling
+    end
+
+    def grand_total
+    end
+
+    def proceed_with_order
+    end
+
+    def return_to_home_page
+    end
+end
